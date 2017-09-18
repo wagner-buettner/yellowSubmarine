f38cbdb06aae5802a5cd59b772a1462d9a50fc95
Wagner Morais Domingues <wagner.morais@gmail.com>
2017-09-18 13:10:56 -0300

    Casca da classe controller/Move

---
+++ src/main/java/br/com/buscape/yellowSubmarine/controller/Move.java
@@ -0,0 +1,22 @@
+package br.com.buscape.yellowSubmarine.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author wagner on 18/09/17
+ */
+public class Move {
+
+    private static final Map<String,Comando> actions;
+
+    static{
+        actions = new HashMap<String, Comando>();
+
+    }
+
+    public String applyRule(String inputCoordinates) {
+
+    }
+
+}
